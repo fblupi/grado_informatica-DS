353dab24afc4b94e0282210a5ff88547708cb48d
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2015-03-01 18:48:42 +0100

    cambios en el main: cliente usa dos visitantes

--- P1/src/Cliente.java
+++ P1/src/Cliente.java
@@ -2,15 +2,20 @@
 public class Cliente {
 	public static void main(String[] params) {
 		// ¿paso en el constructor el precio y la potencia o vienen ya dados?
-		Disco d = new Disco("Seagate K139",100,400); 
-		Tarjeta t = new Tarjeta("Kingston L194",200,300);
-		Bus b = new Bus("Alsina (?)",10,50);
-		VisitantePrecio vp = new VisitantePrecio();
+		Equipo d = new Disco("Seagate K139",100,400); 
+		Equipo t = new Tarjeta("Kingston L194",200,300);
+		Equipo b = new Bus("Alsina (?)",10,50);
+		VisitanteEquipo vp = new VisitantePrecio();
+		VisitanteNombre vn = new VisitanteNombre();
 		
 		d.aceptar(vp);
 		t.aceptar(vp);
 		b.aceptar(vp);
 		
-		System.out.println("El precio total es: " + vp.precio());
+		System.out.println("Precio sin descuento: " + ((VisitantePrecio)vp).precio()
+				+ "\nComponentes comprados: ");
+		d.aceptar(vn);
+		t.aceptar(vn);
+		b.aceptar(vn);
 	}
 }
