c913ed1d9fd86bdb1654a87aa2c2c32ac71fd884
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2015-04-25 16:58:28 +0200

    Opción 2 de cargar sonido (comentada)
    No soluciona el "fallo"

--- P2/CinemaQuiz/app/src/main/java/com/practicas_ds/cinemaquiz/GameActivity.java
+++ P2/CinemaQuiz/app/src/main/java/com/practicas_ds/cinemaquiz/GameActivity.java
@@ -8,6 +8,7 @@ import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -225,6 +226,18 @@ public class GameActivity extends Activity {
                         mp.start();
                 }
             });
+            /* Opción 2
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            try {
+                Uri myUri = Uri.parse("android.resource://" + getPackageName() + "/" + getSongId(this, this.pregunta.getRecurso()));
+                mediaPlayer.setDataSource(this, myUri);
+                mediaPlayer.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mediaPlayer.start();
+            */
             play.setEnabled(false); // Se deshabilita el botón play, pues actualmente está sonando
         }
 
